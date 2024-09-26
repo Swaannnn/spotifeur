4c665a1448fc9aea2aa9fefbe19caef977eaf382
Swann Gillé <swanngille@gmail.com>
2024-09-26 15:56:58 +0200

    modif authOptions pour build

---
+++ src/app/api/spotify/profile/recently-played/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { spotifyApi } from "@/lib/spotify";
+
+export async function GET(req: NextRequest) {
+    try {
+        const token = await getToken({req});
+        if (!token) {
+            return NextResponse.json({error: 'Unauthorized'}, {status: 401});
+        }
+
+        spotifyApi.setAccessToken(token.accessToken as string);
+
+        const response = await spotifyApi.getMyRecentlyPlayedTracks()
+        return NextResponse.json(response.body);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des titres récemment écoutés:', error);
+        return NextResponse.json({ error: 'Failed to fetch profile' }, { status: 500 });
+    }
+}
---
+++ src/lib/authOptions.ts
@@ -0,0 +1,68 @@
+import { NextAuthOptions } from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import { spotifyApi } from "@/lib/spotify";
+import { JWT } from "next-auth/jwt";
+
+async function refreshAccessToken(token: JWT) {
+    try {
+        spotifyApi.setAccessToken(token.accessToken as string);
+        spotifyApi.setRefreshToken(token.refreshToken as string);
+
+        const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+        return {
+            ...token,
+            accessToken: refreshedToken.access_token,
+            expires_at: Date.now() + refreshedToken.expires_in * 1000,
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        };
+    } catch (error) {
+        console.error("Erreur lors du rafraîchissement du token :", error);
+        return { ...token, error: "RefreshAccessTokenError" };
+    }
+}
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID!,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+            authorization:
+                "https://accounts.spotify.com/authorize?scope=playlist-modify-private,user-top-read,user-read-recently-played",
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token as string | undefined;
+                token.refreshToken = account.refresh_token as string | undefined;
+                const expiresIn = account.expires_in as number | undefined;
+                if (expiresIn) {
+                    token.expires_at = Date.now() + expiresIn * 1000;
+                }
+            }
+
+            if (Date.now() > (token.expires_at as number)) {
+                token.error = "AccessTokenExpired";
+                const newToken = await refreshAccessToken(token);
+                if (newToken.error) {
+                    return { ...newToken, error: "RefreshAccessTokenError" };
+                }
+                return newToken;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken as string | undefined;
+            session.refreshToken = token.refreshToken as string | undefined;
+            session.error = token.error as string | undefined;
+            return session;
+        },
+    },
+    pages: {
+        signIn: '/login',
+        signOut: '/login',
+        // error: '/auth/error',
+    }
+};
